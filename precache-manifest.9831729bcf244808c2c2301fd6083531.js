57a73a4f3e19ab7bc77992173f7800bd9c3f4f50
zirui2333 <zhengzirui43@gmail.com>
2024-11-22 12:13:58 -0500

    Updates

---
+++ precache-manifest.9831729bcf244808c2c2301fd6083531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ae482fe79917463f19e9637f039e21",
+    "url": "/zirui2333/Assignment_4_datawrapper/index.html"
+  },
+  {
+    "revision": "8555e2b961f47486d5d0",
+    "url": "/zirui2333/Assignment_4_datawrapper/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/zirui2333/Assignment_4_datawrapper/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/zirui2333/Assignment_4_datawrapper/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "8555e2b961f47486d5d0",
+    "url": "/zirui2333/Assignment_4_datawrapper/static/js/main.5da190ee.chunk.js"
+  },
+  {
+    "revision": "8571c79aa871109f32b0",
+    "url": "/zirui2333/Assignment_4_datawrapper/static/js/runtime~main.7829a7d0.js"
+  }
+]);
